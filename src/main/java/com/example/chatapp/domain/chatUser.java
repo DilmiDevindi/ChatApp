fa36cfd3caff169ea8454eced20604383bce7df7
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-05-09 03:57:04 +0530

    Add getters, setters, and toString method to ChatUser
    This commit introduces getter and setter methods for all fields in the `ChatUser` class, enhancing encapsulation and enabling controlled access to the class properties. Additionally, a `toString` method has been implemented to provide a readable string representation of the object for debugging and logging purposes.

--- src/main/java/com/example/chatapp/domain/chatUser.java
+++ src/main/java/com/example/chatapp/domain/chatUser.java
@@ -50,4 +50,82 @@ public class ChatUser implements Serializable {
         this.email = email;
         this.isAdmin = isAdmin;
     }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", profilePicture='" + profilePicture + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", isOnline=" + isOnline +
+                '}';
+    }
 }
