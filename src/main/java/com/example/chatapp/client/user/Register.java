af783aacf80e063cb55a18c40bd126384b054f90
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-05-09 14:43:59 +0530

    Add imports for RMI and GUI components in Register.java
    Added necessary import statements for RMI functionality, such as LocateRegistry and Registry, and for GUI components like Swing and AWT. These changes prepare the Register.java class to handle remote method invocations and graphical user interface operations.

--- src/main/java/com/example/chatapp/client/user/Register.java
+++ src/main/java/com/example/chatapp/client/user/Register.java
@@ -1,5 +1,18 @@
 package com.example.chatapp.client.user;
 
+import com.example.chatapp.domain.ChatUser;
+import com.example.chatapp.rmi.UserService;
+import com.example.chatapp.server.implementation.Server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Register {
 
 }
